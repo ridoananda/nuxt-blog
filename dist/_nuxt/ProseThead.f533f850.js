29fbaea8968691baa3f944cecc42329e6b6619fc
Rido Ananda <ridoananda123@gmail.com>
2022-10-29 09:29:50 +0700

    deploy

---
+++ dist/_nuxt/ProseThead.f533f850.js
@@ -0,0 +1,1 @@
+import{e as r,o,f as t,r as s}from"./entry.ae168f48.js";const a={};function n(e,c){return o(),t("thead",null,[s(e.$slots,"default")])}const f=r(a,[["render",n]]);export{f as default};
